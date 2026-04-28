75972c303bf9960a6627c04e3c379a983850e00f
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-04-28 18:59:19 +0530

    Integrate Kafka producer for real-time location updates via socket.io

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import path from "node:path";
 import express from "express";
 import { Server } from "socket.io";
 
+import { kafkaClient } from "./kafka-client.js";
+
 async function main() {
   const PORT = process.env.PORT ?? 8000;
 
@@ -11,17 +13,30 @@ async function main() {
   const server = http.createServer(app);
   const io = new Server();
 
+  const kafkaProducer = kafkaClient.producer();
+  await kafkaProducer.connect();
+
   io.attach(server);
 
   io.on("connection", (socket) => {
     console.log(`[Socket:${socket.id}]: Connected Success...`);
 
-    socket.on("client:location:update", (locationData) => {
+    socket.on("client:location:update", async (locationData) => {
       const { latitude, longitude } = locationData;
       console.log(
         `[Socket:${socket.id}]:client:location:update:`,
         locationData,
       );
+
+      await kafkaProducer.send({
+        topic: "location-updates",
+        messages: [
+          {
+            key: socket.id,
+            value: JSON.stringify({ id: socket.id, latitude, longitude }),
+          },
+        ],
+      });
     });
   });
 
